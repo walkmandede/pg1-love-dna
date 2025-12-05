e752293f756afb44cb44e976d7b545f4a2e82406
Eddie <walkmandede@gmail.com>
2025-12-06 03:46:29 +0700

    update

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/kyawphyoehan/Desktop/flutter_projects/pg1_question/pg1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/kyawphyoehan/Desktop/flutter_projects/pg1_question/pg1/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/kyawphyoehan/Desktop/flutter_projects/pg1_question/pg1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/kyawphyoehan/Desktop/flutter_projects/pg1_question/pg1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
